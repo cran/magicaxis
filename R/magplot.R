bff60505624a3b7b175953479a00cb2acf34e2a6
Aaron Robotham <aaron.robotham@uwa.edu.au>
2015-06-10 00:00:00 +0000

    version 1.9.4

--- R/magplot.R
+++ R/magplot.R
@@ -1,16 +1,7 @@
 magplot <-
-function(x, y, log='', xlab=NULL, ylab=NULL, unlog='Auto', majorn=5, minorn=5, main='', labels=TRUE, crunch=TRUE, logpretty=TRUE, prettybase=10, hersh=FALSE, family='sans',side=1:2,frame.plot=TRUE,...){
-if(missing(y)) plot(x,axes=F,xlab='',ylab='',main=main,log=log,frame.plot=frame.plot,...)
-else plot(x,y,axes=F,xlab='',ylab='',main=main,log=log,frame.plot=frame.plot,...)
+function(x, y, log='', main='', side = 1:2, majorn = 5, minorn = 5, tcl = 0.5, ratio = 0.5, labels = TRUE, unlog = "Auto", mgp = c(2,0.5,0), mtline = 2, xlab = NULL, ylab = NULL, crunch = TRUE, logpretty = TRUE, prettybase = 10, hersh = FALSE, family = "sans", frame.plot=TRUE, usepar=FALSE, ...){
+if(missing(y)) plot(x, axes=F, xlab='', ylab='', main=main, log=log, frame.plot=FALSE, ...)
+else plot(x, y, axes=F, xlab='', ylab='', main=main, log=log, frame.plot=FALSE, ...)
 
-#currentbty=par()$bty
-#if(all(side %in% c(1,2,3,4) & all(c(1,2,3,4) %in% side))){par(bty='o')}
-#if(all(side %in% c(1,2) & all(c(1,2) %in% side))){par(bty='L')}
-#if(all(side %in% c(3,4) & all(c(3,4) %in% side))){par(bty='7')}
-#if(all(side %in% c(1,2,3) & all(c(1,2,3) %in% side))){par(bty='C')}
-#if(all(side %in% c(1,2,4) & all(c(1,2,4) %in% side))){par(bty='U')}
-#if(all(side %in% c(1,3,4) & all(c(1,3,4) %in% side))){par(bty=']')}
-
-magaxis(side=side,majorn=majorn,minorn=minorn,xlab=xlab,ylab=ylab,labels=labels,unlog=unlog,box=F,crunch=crunch,logpretty=logpretty,prettybase=prettybase,hersh=hersh,family=family)
-#par(bty=currentbty)
+if(side[1] !=FALSE){magaxis(side = side, majorn = majorn, minorn = minorn, tcl = tcl, ratio = ratio, labels = labels, unlog = unlog, mgp = mgp, mtline = mtline, xlab = xlab, ylab = ylab, crunch = crunch, logpretty = logpretty, prettybase = prettybase, hersh = hersh, family = family, frame.plot = frame.plot, usepar = usepar)}
 }
